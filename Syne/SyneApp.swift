c0bd74753c075e51aa358b7e13b7c07b6e9d823c
damima <davidedamima@gmail.com>
2025-05-22 21:54:45 +0200

    Initial Commit

---
+++ Syne/SyneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyneApp.swift
+//  Syne
+//
+//  Created by Davide on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SyneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SyneTests/SyneTests.swift
@@ -0,0 +1,17 @@
+//
+//  SyneTests.swift
+//  SyneTests
+//
+//  Created by Davide on 22/05/25.
+//
+
+import Testing
+@testable import Syne
+
+struct SyneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
